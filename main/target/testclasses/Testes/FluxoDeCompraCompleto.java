b75d1ebc5c72f4235f776acb592646b36b0f76d9
Rafaela Lyns <rafaela.debem@hotmail.com>
2023-11-22 20:07:40 -0300

    Criação de teste.

--- main/target/testclasses/Testes/FluxoDeCompraCompleto.java
+++ main/target/testclasses/Testes/FluxoDeCompraCompleto.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
 import main.target.classe.Framework.TestBase;
+import main.target.testclasses.Tasks.TaskLogin;
 
 public class FluxoDeCompraCompleto extends TestBase{
 	
 	private WebDriver driver = getDriverManager();
+	private TaskLogin taskLogin;
 
 	@Test
 	public void fluxoCompletoDeCompra() {
-		
+		taskLogin.efetuarLogin();
 	}
 }
